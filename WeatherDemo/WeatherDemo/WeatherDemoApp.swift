20cfbbb0abb99eebcce33e69b3f2197a14863637
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2022-09-21 14:49:24 -0400

    created project structure and added model files

---
+++ WeatherDemo/WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by AlecNipp on 9/21/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
